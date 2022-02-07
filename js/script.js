75452781115d312dfb988226da26d150a7688555
Gabrielbe17 <gabibascope@gmail.com>
2022-02-07 21:22:42 +0100

    added ham menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-const toggleMenu = document.getElementById('toggle-menu');
-toggleMenu.addEventListener('click', () => {
-    alert('Still working on it :(');
-})
+
+const menu = document.getElementById('nav-links');
+const menuItems = document.getElementsByClassName('menuItem');
+const hamburger= document.getElementById('hamburger-menu');
+const closeIcon= document.getElementById('close-icon');
+const menuIcon = document.getElementById('menuIcon');
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "flex";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+
+  
